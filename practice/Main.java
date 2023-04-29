e7b538b84edcb02133238ffb8257296a486f9d41
Ted Yuan <tedyuan@TedMBP.local>
2023-04-29 00:40:23 -0400

    preparing for lab2/proj1

---
+++ practice/Main.java
@@ -0,0 +1,15 @@
+import org.graalvm.compiler.core.common.util.IntList;
+
+public class Main{
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r){
+        first = f;
+        rest = r;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
